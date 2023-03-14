e18cf9defdc904199f6122a8c483a52b2a071d58
CESLTD\vijayalakshmi.surend <vijayalakshmi.surendran@cesltd.com>
2023-03-14 19:20:21 +0530

    first commit

---
+++ Models/VMLogin.cs
@@ -0,0 +1,13 @@
+﻿namespace AutthProject.Models
+{
+    public class VMLogin
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool KeepLoggedIn { get; set; }
+
+
+    }
+}
